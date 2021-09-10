f71e0dd7c8ca9cccce5af781d60104f13f9cb578
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-10 21:17:47 +0300

    Add tak03 to dz08

--- dz08/task03/main.go
+++ dz08/task03/main.go
@@ -2,56 +2,59 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 /*
-Задание 1:
-Пользователь вводит месяц, программа должна вывести, на какое время года
-(зима, весна, лето, осень) этот месяц выпадает.
+В киоске с лимонадом, каждый лимонад стоит 5 долларов.
+Клиенты стоят в очереди, чтобы купить у вас, и заказывают по одному (в порядке, указанном в счетах).
+Каждый покупатель может купить только один лимонад и заплатить купюрой на 5, 10 или 20 долларов.
+Вы должны дать каждому покупателю сдачу с его купюры.
 */
 
 func main() {
+	var bill5, bill10, bill20 int
+	var bills string
+	var lemonadeChange bool
+	fmt.Println("В киоске с лимонадом, каждый лимонад стоит 5 долларов.")
 
-	var bills int
-	fmt.Println("====================")
-	fmt.Println("======================")
-	_, _ = fmt.Scanf("%v", &bills)
-	for {
-		i := 1
-		i++
-		switch bills {
-		case ',':
-			fmt.Printf("\n")
-		default:
+		fmt.Println("Введите, через запятую, какие купюры у покупателей (в порядке, указанном в счетах):")
+		_, _ = fmt.Scan(&bills)
+		bill := strings.Split(bills, ",")
+		for i:= 0; i < len(bill); i++ {
+			switch {
+			case bill [i]== "5":
+				bill5++
+				lemonadeChange = true
+			case bill [i]== "10":
+				bill10++
+				bill5 --
+				if bill5 >= 0 {
+					lemonadeChange = true
+				} else {
+					lemonadeChange = false
+				}
 
-			fmt.Printf("%T", bills)
-
+			case bill [i]== "20":
+				bill20++
+				if bill5 - 1 >= 0 && bill10  - 1 >= 0 {
+					bill5 --
+					bill10 --
+					lemonadeChange = true
+				} else if bill5 - 3 >= 0{
+					bill5 -= 3
+					lemonadeChange = true
+				} else {
+					lemonadeChange = false
+				}
+			default:
+				lemonadeChange = false
+				fmt.Println(lemonadeChange, "bills only 5,10,20")
+				return
+			}
 		}
-		if i > 10 {
-			break
-		}
-
-		/*var lemonadeChange (bills[]int) = false
-
-		switch userDay {
-		case "пн":
-			fmt.Println("понедельник")
-			fallthrough
-		case "вт":
-			fmt.Println("вторник")
-			fallthrough
-		case "ср":
-			fmt.Println("среда")
-			fallthrough
-		case "чт":
-			fmt.Println("четверг")
-			fallthrough
-		case "пт":
-			fmt.Println("пятница")
-		case "сб", "вс":
-			fmt.Println("выходные!")
-		default:
-			fmt.Println("Такого дня нет!")
-		}*/
-	}
+	fmt.Println(lemonadeChange)
+		summ := bill5*5 + bill10*10 + bill20*20
+	fmt.Print("Мы заработали: ",summ, "$\n")
 }
+
