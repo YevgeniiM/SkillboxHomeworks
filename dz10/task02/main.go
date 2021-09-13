ee919c1e17dbdae12b87aad3a69b4156d935c907
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-13 16:06:47 +0300

    Add task02 to dz10

--- dz10/task02/main.go
+++ dz10/task02/main.go
@@ -6,22 +6,23 @@ import (
 )
 
 func main() {
-	var percent, bankProfit, debit, term, sumPercentPerMonth, factSumPercentPerMonth float64
+	var percent, bankProfit, debit, term, sumPercentPerMonth, factSumPercentPerMonth, sum float64
 
 	fmt.Println("Введите сумму депозита:")
-	_, _ = fmt.Scan(&debit)
+	_, _ = fmt.Scan(&sum)
 	fmt.Println("Введите ежемесячный процент капитализаци:")
 	_, _ = fmt.Scan(&percent)
 	fmt.Println("Введите количество лет на которое делается вклад:")
 	_, _ = fmt.Scan(&term)
-	//debit *= 100
+	debit = sum * 100
 	for i := 1; i <= int(term*12); i++ {
-		sumPercentPerMonth = debit * percent / 100                        //
-		factSumPercentPerMonth = math.Floor(sumPercentPerMonth*100) / 100 //float64(int(debit*percent)/100)
+		sumPercentPerMonth = debit * percent / 100              //
+		factSumPercentPerMonth = math.Floor(sumPercentPerMonth) //float64(int(debit*percent)/100)
 		bankProfit += sumPercentPerMonth - factSumPercentPerMonth
 		debit += factSumPercentPerMonth
 	}
-	fmt.Print("К концу срока вклада  сумма депозита составит: ", math.Ceil(debit*100)/100, "грн.\n")
-	fmt.Print("Прибыль банка от округлений составит: ", bankProfit, "грн.\n")
+	debit = math.Round(debit*100) / 100
+	fmt.Printf("Для %v рублей, %v%% и %v года программа должна вывести %.2f и \n", sum, percent, term, debit/100)
+	fmt.Print(bankProfit/100, " (возможно меньше знаков) \n")
 
 }
