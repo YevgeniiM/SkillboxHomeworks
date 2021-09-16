70c25048ea00d11f2629b0bfb43e12da23bc364d
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-16 15:25:42 +0300

    Add RandomNumber to dz00

--- dz00/mars/RandomNuber/main.go
+++ dz00/mars/RandomNuber/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	var num = rand.Intn(10) + 1
-	fmt.Println(num)
 
-	num = rand.Intn(10) + 1
-	fmt.Println(num)
+	day := time.Now().Weekday()
+	fmt.Println(day)
+	t := time.Now().Unix()
+	fmt.Printf("Текущий timestamp %d\n", t)
+	delta := t % 10
+	fmt.Println(delta)
+	lucky := rand.Intn(int(delta + 5))
+	fmt.Println(lucky)
 }
