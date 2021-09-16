c90296f9a4f214f4381104b184d6c7b73a136224
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-16 15:27:51 +0300

    Add mars to dz00

--- dz00/mars/main.go
+++ dz00/mars/main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+
+	message := "uv vagreangvbany fcnpr fgngvba"
+
+	for i := 0; i < len(message); i++ { // Итерирует каждый символ ASCII
+		c := message[i]
+		if c >= 'a' && c <= 'z' { // Оставляет оригинальную пунктуацию и пробелы
+			c = c + 13
+			if c > 'z' {
+				c = c - 26
+			}
+		}
+		fmt.Printf("%c", c)
+
+	}
+	fmt.Printf("\n%-15v $%4v\n", "SpaceX", 94)
 }
