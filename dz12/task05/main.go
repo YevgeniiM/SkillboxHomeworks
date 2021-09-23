ef4f7df88138b1980c5daa05523290a8bd5f452f
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-23 14:48:16 +0300

    Add task05 to dz12

--- dz12/task05/main.go
+++ dz12/task05/main.go
@@ -10,27 +10,6 @@ func main() {
 	/*
 		переписать задачи из урока 2 и 3 на пакет ioutil
 	*/
-
-	//f, err := os.Open("Log.txt")
-	//if err != nil {
-	//	fmt.Println("Not open file", err)
-	//	return
-	//}
-	//defer f.Close()
-	//
-	//fi, err := f.Stat()
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
-	//size := fi.Size()
-	//buf := make([]byte, size)
-	//_, err = f.Read(buf)
-	//if err != nil {
-	//	fmt.Println("Not red file", err)
-	//	return
-	//}
-	//fmt.Println(string(buf))
 	fileName := "Log.txt"
 	file, err := os.Open(fileName)
 	if err != nil {
