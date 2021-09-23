2e0a57c79edd5442db520db6081e17b829d2d2ed
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-23 14:49:49 +0300

    Add task01 to dz12

--- dz12/task01/main.go
+++ dz12/task01/main.go
@@ -19,27 +19,7 @@ func main() {
 	file.WriteString("Программа выводит сообщения в формате:\n№ строки, дата, сообщение \n")
 
 	in := bufio.NewReader(os.Stdin)
-	//var message string
-	//var codLit int
-
 	for j := 1; ; j++ {
-		//message = ""
-		//fmt.Println("Введите сообщение:")
-		//for { // собитаем строку посимвольно через unicod до символа переноса строки
-		//	_, _ = fmt.Scanf("%c", &codLit)
-		//	if string(codLit) == "\n" {
-		//		break
-		//	}
-		//	message += string(codLit)
-		//	//fmt.Printf("%s", message)
-		//}
-		//if message == "выход" {
-		//	file.WriteString("Вышли из программы \n")
-		//	return
-		//} else {
-		//	dt := time.Now()
-		//	file.WriteString(fmt.Sprintf("№ %-3d %s    %s \n", j, dt.Format("01-02-2006 15:04:05"), message))
-
 		fmt.Println("Введите сообщение:")
 		text, _ := in.ReadString('\n')
 		switch text {
