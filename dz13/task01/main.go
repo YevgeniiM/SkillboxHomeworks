50c3e8767c5a4814fa080c56aad477b40e6ff0da
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-27 17:37:47 +0300

    Add task01 to dz13

--- dz13/task01/main.go
+++ dz13/task01/main.go
@@ -3,31 +3,30 @@ package main
 import "fmt"
 
 func little1(x *int) {
-	*x++
+	*x += 10
 	fmt.Println("first func")
 }
-func little2(s *string) {
-	*s = "Значение переменной равно"
+func little2(x *int) {
+	*x /= 2
 	fmt.Println("second func")
 }
 
-func big(little1 func(*int), little2 func(*string)) {
-	x := 2
-	s := ""
-	little2(&s)
-	little1(&x)
-	fmt.Printf("%s : %d\n", s, x)
+func big(x *int, little1 func(*int), little2 func(*int)) {
+	little2(x)
+	little1(x)
+
 }
 
 func main() {
-	a := 10
-	b := ""
+	a := 2
+	x := &a
 
-	little1(&a)
+	little1(x)
 	fmt.Println(a)
 
-	little2(&b)
-	fmt.Println(b)
+	little2(x)
+	fmt.Println(a)
 
-	big(little1, little2)
+	big(x, little1, little2)
+	fmt.Println(a)
 }
