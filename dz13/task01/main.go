8e48f3d278c38302fc5f5aa52a05b25bba15025b
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-09-27 17:28:35 +0300

    Add task01 to dz13

--- dz13/task01/main.go
+++ dz13/task01/main.go
@@ -7,7 +7,7 @@ func little1(x *int) {
 	fmt.Println("first func")
 }
 func little2(s *string) {
-	*s = "Значение переменной x равно"
+	*s = "Значение переменной равно"
 	fmt.Println("second func")
 }
 
@@ -16,9 +16,18 @@ func big(little1 func(*int), little2 func(*string)) {
 	s := ""
 	little2(&s)
 	little1(&x)
-	fmt.Printf("%s : %d", s, x)
+	fmt.Printf("%s : %d\n", s, x)
 }
 
 func main() {
+	a := 10
+	b := ""
+
+	little1(&a)
+	fmt.Println(a)
+
+	little2(&b)
+	fmt.Println(b)
+
 	big(little1, little2)
 }
