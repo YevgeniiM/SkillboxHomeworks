bb229f507e427b480416966005dd59f5e08fd0e3
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-10-04 14:38:56 +0300

    Add task02 to dz20

--- dz20/task02/main.go
+++ dz20/task02/main.go
@@ -1,40 +1,50 @@
 package main
 
-import "fmt"
-
-/*
-Задание 1. Подсчёт определителя
-Что нужно сделать
-Напишите функцию, вычисляющую определитель матрицы размером 3 × 3.
-det(A)= a_{11}a_{22}a_{33}+a_{12}a_{23}a_{31}+a_{13}a_{21}a_{32}-a_{13}a_{22}a_{31}-a_{11}a_{23}a_{32}-a_{12}a_{21}a_{33}
-*/
-const (
-	rows = 3
-	cols = 3
+import (
+	"fmt"
 )
 
-func det(m [rows][cols]int) (detM int) {
-	var x, y int
-	//
-	for j := 0; j < cols; j++ {
-		plusDet := 1
-		minusDet := 1
-		for i := 0; i < rows; i++ {
-			y = (i + j) % cols  // по правилу Саррюса присходит эфект переполнения номеров столбцов
-			plusDet *= m[i][y]  // перемножаем значения "+ диагонали"
-			x = rows - 1 - i    // значения "- диагонали" беме из левого нижнего угла к правому верхнему
-			minusDet *= m[x][y] // перемножаем значения "- диагонали"
+/*
+Задание 2. Умножение матриц
+Что нужно сделать
+Напишите функцию, умножающую две матрицы размерами 3 × 5 и 5 × 4.
+*/
+const (
+	rows       = 3
+	RowsToCols = 5
+	cols       = 4
+)
+
+func multiplicationM(m1 [rows][RowsToCols]int, m2 [RowsToCols][cols]int) (m3 [rows][cols]int) {
+	var row, col int
+	for i := 0; i < rows*cols; i++ {
+		row = i / cols
+		col = i % cols
+		for j := 0; j < RowsToCols; j++ {
+			m3[row][col] += m1[row][j] * m2[j][col]
 		}
-		detM += plusDet - minusDet
 	}
-	return
+	return m3
 }
+
 func main() {
-	matrix := [rows][cols]int{
-		{7, 10, 6},
-		{2, 21, 8},
-		{3, 15, 10},
+	matrix1 := [rows][RowsToCols]int{
+		{7, 10, 6, 12, 25},
+		{2, 15, 8, 10, 11},
+		{3, 15, 10, 45, 1},
 	}
-	det := det(matrix)
-	fmt.Println(det)
+	matrix2 := [RowsToCols][cols]int{
+		{7, 10, 6, 12},
+		{2, 15, 8, 10},
+		{3, 15, 10, 45},
+		{5, 2, 74, 4},
+		{41, 5, 9, 7},
+	}
+	multiMatrix := multiplicationM(matrix1, matrix2)
+	fmt.Println(multiMatrix)
+	fmt.Println("===================")
+
+	for i := 0; i < len(multiMatrix); i++ {
+		fmt.Println(multiMatrix[i])
+	}
 }
