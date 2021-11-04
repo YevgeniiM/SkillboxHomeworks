b435ec14e11ff2af8c5f8b299a9085d5d8ffe682
Yevhenii Molodtsov <perga.in.ua@gmail.com>
2021-11-04 11:10:01 +0200

    Add task01 to dz26

--- dz26/task01/main.go
+++ dz26/task01/main.go
@@ -19,44 +19,78 @@ import (
 два соединённых файла в результирующий.
 */
 
-func readFile(fileName string) []byte {
+func readFile(fileName string) ([]byte, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer file.Close()
 	factBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return factBytes
+	return factBytes, nil
 
 }
-func makeResultFile(first, second []byte, fileName string) {
+func makeResultFile(first, second []byte, fileName string) error {
 	file, err := os.Create(fileName)
 	if err != nil {
-		return
+		return err
 	}
 	defer file.Close()
-	file.WriteString(string(first))
-	file.WriteString(string(second))
+	_, err = file.WriteString(string(first))
+	if err != nil {
+		return err
+	}
+	_, err = file.WriteString(string(second))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
+	var f1, f2 []byte
+	var err error
 	//fmt.Println(len(os.Args), os.Args)
 
 	if len(os.Args) == 2 {
-		fmt.Println(string(readFile(os.Args[1])))
+		f1, err = readFile(os.Args[1])
+		if err != nil {
+			fmt.Printf("Не смогли открыть файл: %v\n", err)
+		}
+		fmt.Println(string(f1))
 	} else if len(os.Args) == 3 {
-		f := fmt.Sprintf(string(readFile(os.Args[1])) + string(readFile(os.Args[2])))
-		f1 := strings.Split(f, "\n")
-		fmt.Println(f1)
+		f1, err = readFile(os.Args[1])
+		if err != nil {
+			fmt.Printf("Не смогли открыть файл: %v\n", err)
+		}
+		f2, err = readFile(os.Args[2])
+		if err != nil {
+			fmt.Printf("Не смогли открыть файл: %v", err)
+		}
+		f := fmt.Sprintf(string(f1) + string(f2))
+		f3 := strings.Split(f, "\n")
+		fmt.Println(f3)
 		fmt.Println("-------------------------------")
-		fmt.Println(strings.Join(f1, "\n"))
+		fmt.Println(strings.Join(f3, "\n"))
 	} else if len(os.Args) == 4 {
-		fmt.Println(os.Args[1], os.Args[2], os.Args[3])
-		makeResultFile(readFile(os.Args[1]), readFile(os.Args[2]), os.Args[3])
+		f1, err = readFile(os.Args[1])
+		if err != nil {
+			fmt.Printf("Не смогли открыть файл: %v\n", err)
+		}
+		f2, err = readFile(os.Args[2])
+		if err != nil {
+			fmt.Printf("Не смогли открыть файл: %v\n", err)
+		}
+
+		err = makeResultFile(f1, f2, os.Args[3])
+		if err != nil {
+			fmt.Printf("Не смогли создать результирующий файл: %v\n", err)
+		} else {
+			fmt.Printf("Информация из файлов %v и %v запмсана в результирующий файл %v \n", os.Args[1], os.Args[2], os.Args[3])
+		}
 
 	} else {
 		fmt.Println("Ошибка. Программа должна быть запущена с аргументами в колличестве от 1 до 3")
